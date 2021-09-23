98587b26c7f572f4579f7a23ebd070574e9cd855
Paramjeet Arora <gagan@Paramjeets-Air.cgocable.net>
2021-09-23 07:24:43 -0400

    beeping

---
+++ timer1.js
@@ -0,0 +1,14 @@
+/*Implement an alarm clock / timer which will beep after a specified amount of time has passed.
+  The user can specify an unlimited number of alarms using command line arguments
+*/
+
+// assume input time is in second
+const inputArgs = process.argv
+  .slice(2)
+  
+  console.log(inputArgs);
+inputArgs.forEach((time) => {
+  setTimeout(() => {
+    process.stdout.write('\x07');
+  }, time );
+});
