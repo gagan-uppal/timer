45eaf08515bca841c2a919c3a4eb659116bcb637
Gagandeep Uppal <guppal.profile@gmail.com>
2021-09-26 20:20:52 +0000

    changes made

--- timer1.js
+++ timer1.js
@@ -3,17 +3,31 @@
 */
 
 // assume input time is in second
-const inputArgs = process.argv
-  .slice(2)
+const inputArgs = process.argv.slice(2);
   
-  console.log(inputArgs);
+//console.log(inputArgs);
   
 inputArgs.forEach((time) => {
   //if(inputArgs[time] >= 0 && inputArgs[time] !== NaN) {
+    let time1 = Number(time); // converted time to number   => 3,5, NaN
+
+    //if time1 is anumber or not with isNan condition => false, false, true//
+    let time2 = isNaN(time1);  
+    // if it is not NaN
+    if(time2 === false){
+      
+    }
+
+
+  if((Number.isNaN(Number(time)) !== true) && (Number(time) >= 0)){
 
     setTimeout(() => {
-      process.stdout.write('\x07');
-    }, time );
+      //process.stdout.write('\x07');
+        process.stdout.write(`\x07 ${time} `);
+
+    }, time * 1000 );
   //}
+  }
+    
   
 });
